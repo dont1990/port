4d38aef9ae9c08159dd6ef3bea4aa52d5f51552e
4006 <4006@4006.com>
2025-08-05 15:49:10 +0330

    select option ui should be better

--- frontend/app/components/admin/section-header/index.tsx
+++ frontend/app/components/admin/section-header/index.tsx
@@ -1,6 +1,16 @@
+"use client";
+
 import React from "react";
 import { CardHeader, CardTitle } from "../../ui/card";
 import { useLang } from "@/app/context/langContext";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/app/components/ui/select";
+import { useTranslation } from "react-i18next";
 
 type Props = {
   title: string;
@@ -8,18 +18,31 @@ type Props = {
 
 const AdminSectionHeader = ({ title }: Props) => {
   const { lang, setLang } = useLang();
+  const { t } = useTranslation("dashboard");
 
   return (
     <CardHeader className="flex flex-row justify-between items-center">
       <CardTitle>{title}</CardTitle>
-      <select
+      <Select
         value={lang}
-        onChange={(e) => setLang(e.target.value as "en" | "fa")}
-        className="border rounded px-2 py-1 text-sm"
+        onValueChange={(value: "en" | "fa") => setLang(value)}
       >
-        <option value="en">English</option>
-        <option value="fa">Persian</option>
-      </select>
+        <SelectTrigger
+          className={`w-[140px] ${
+            lang === "fa" ? "flex-row-reverse" : "flex-row"
+          }`}
+        >
+          <SelectValue placeholder={t("language.SelectLanguage")} />
+        </SelectTrigger>
+        <SelectContent>
+          <SelectItem className="text-start" value="en">
+            {t("language.English")}
+          </SelectItem>
+          <SelectItem className="text-start" value="fa">
+            {t("language.Persian")}
+          </SelectItem>
+        </SelectContent>
+      </Select>
     </CardHeader>
   );
 };
